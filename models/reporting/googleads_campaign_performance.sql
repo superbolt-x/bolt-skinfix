0a158c3a584bed8406f1b262bc2333e1705da2ec
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-11-21 11:06:17 -0500

    Create googleads_campaign_performance.sql

---
+++ models/reporting/googleads_campaign_performance.sql
@@ -0,0 +1,28 @@
+{{ config (
+    alias = target.database + '_googleads_campaign_performance'
+)}}
+
+SELECT 
+account_id,
+campaign_name,
+campaign_id,
+campaign_status,
+campaign_type_default,
+CASE WHEN account_id = '7985220394' THEN 'US'
+    WHEN account_id = '4973168899' THEN 'CA'
+END AS market,
+date,
+date_granularity,
+CASE WHEN account_id = '4973168899' THEN spend*0.76
+    ELSE spend
+END as spend,
+impressions,
+clicks,
+conversions as purchases,
+CASE WHEN account_id = '4973168899' THEN conversions_value*0.76
+    ELSE conversions_value
+END as revenue,
+search_impression_share,
+search_budget_lost_impression_share,
+search_rank_lost_impression_share
+FROM {{ ref('googleads_performance_by_campaign') }}
