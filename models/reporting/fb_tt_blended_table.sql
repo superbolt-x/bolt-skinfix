d3b8984b13e8374c5096f876fdc8a827c57b4634
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-11-21 11:12:55 -0500

    Create fb_tt_blended_table.sql

---
+++ models/reporting/fb_tt_blended_table.sql
@@ -0,0 +1,18 @@
+{{ config (
+    alias = target.database + '_fb_tt_blended_table'
+)}}
+
+  (SELECT 'Facebook' as channel, date, date_granularity, market, ad_name, visual, campaign_name, ad_approach, ad_type, campaign_type_custom,
+        COALESCE(SUM(spend),0) AS spend, COALESCE(SUM(impressions),0) AS impressions, COALESCE(SUM(link_clicks),0) AS clicks, COALESCE(SUM(purchases),0) AS purchases, 
+        COALESCE(SUM(revenue),0) AS revenue
+    FROM {{ ref('facebook_ad_performance') }}
+    WHERE account_id = '152712828589300'
+    GROUP BY channel, date, date_granularity, market, ad_name, visual, campaign_name, ad_approach, ad_type, campaign_type_custom)
+    
+    UNION ALL
+    
+    (SELECT 'TikTok' as channel, date, date_granularity, market, ad_name, visual, campaign_name, ad_approach, ad_type, campaign_type_custom,
+        COALESCE(SUM(spend),0) AS spend, COALESCE(SUM(impressions),0) AS impressions, COALESCE(SUM(clicks),0) AS clicks, COALESCE(SUM(purchases),0) AS purchases, 
+        COALESCE(SUM(revenue),0) AS revenue
+    FROM {{ ref('tiktok_ad_performance') }}
+    GROUP BY channel, date, date_granularity, market, ad_name, visual, campaign_name, ad_approach, ad_type, campaign_type_custom)
