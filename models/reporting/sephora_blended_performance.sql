0be585940b469a93ff75c363cd84e4b5fe005716
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-02-12 18:25:14 -0500

    Update sephora_blended_performance.sql
    ad group

--- models/reporting/sephora_blended_performance.sql
+++ models/reporting/sephora_blended_performance.sql
@@ -2,29 +2,29 @@
     alias = target.database + '_sephora_blended_performance'
 )}}
 
-  (SELECT 'Facebook' as channel, date, date_granularity, market, product_name,
+  (SELECT 'Facebook' as channel, date, date_granularity, market, campaign_name, adset_name as ad_group_name, product_name,
         COALESCE(SUM(spend),0) AS spend, COALESCE(SUM(impressions),0) AS impressions, COALESCE(SUM(link_clicks),0) AS clicks, COALESCE(SUM(purchases),0) AS purchases, 
         COALESCE(SUM(revenue),0) AS revenue
     FROM {{ source('reporting','facebook_ad_performance') }}
     WHERE account_id = '152712828589300'
     AND campaign_type_custom = 'Sephora'
-    GROUP BY channel, date, date_granularity, market, product_name)
+    GROUP BY channel, date, date_granularity, market, campaign_name, ad_group_name, product_name)
     
     UNION ALL
     
-    (SELECT 'TikTok' as channel, date, date_granularity, market, product_name,
+    (SELECT 'TikTok' as channel, date, date_granularity, market, campaign_name, adgroup_name as ad_group_name, product_name,
         COALESCE(SUM(spend),0) AS spend, COALESCE(SUM(impressions),0) AS impressions, COALESCE(SUM(clicks),0) AS clicks, COALESCE(SUM(purchases),0) AS purchases, 
         COALESCE(SUM(revenue),0) AS revenue
     FROM {{ source('reporting','tiktok_ad_performance') }}
     WHERE campaign_type_custom = 'Sephora'
-    GROUP BY channel, date, date_granularity, market, product_name)
+    GROUP BY channel, date, date_granularity, market, campaign_name, ad_group_name, product_name)
 
     UNION ALL
     
-    (SELECT 'Youtube Shorts' as channel, date, date_granularity, market, product_name,
+    (SELECT 'Youtube Shorts' as channel, date, date_granularity, market, campaign_name, ad_group_name, product_name,
         COALESCE(SUM(spend),0) AS spend, COALESCE(SUM(impressions),0) AS impressions, COALESCE(SUM(clicks),0) AS clicks, COALESCE(SUM(purchases),0) AS purchases, 
         COALESCE(SUM(revenue),0) AS revenue
     FROM {{ source('reporting','googleads_ad_performance') }}
     WHERE campaign_name ~* 'youtube shorts'
     AND campaign_type_custom = 'Sephora'
-    GROUP BY channel, date, date_granularity, market, product_name)
+    GROUP BY channel, date, date_granularity, market, campaign_name, ad_group_name, product_name)
